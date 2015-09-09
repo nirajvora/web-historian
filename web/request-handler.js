8e51b6a61bd4cc27f1f2eada826596c8d4e48bc7
Hack Reactor Students <accounts@hackreactor.com>
2015-09-09 15:08:27 -0700

    passing both a and b of test 1 and almost done with 2

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,39 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var http = require('./http-helpers');
+var fs = require('fs');
+
+var indexURL = 'web/public/index.html';
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var statusCode = 200;
+
+  fs.readFile(indexURL, function(err, data){
+    if (err) {
+      console.log("ERROR: in handleRequest - readFile of indexURL")
+    } else {
+      res.writeHead(statusCode, http.headers);
+      res.write(data);
+      res.end();
+    }
+  });
+  // console.log(request)
+    // console.log('test1')
+    console.log(req);
+  if (req.method === 'GET' && req.method !== '/'){
+    // console.log('test2')
+    fs.readFile(archive.paths.list, function(err, data) {
+      if (err) {
+        console.log("ERROR: in handleRequest - readFile of indexURL")
+      } else {
+        // console.log('test3')
+        res.end(data);
+      }
+    });
+  }
 };
+
+
+
+
